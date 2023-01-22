9956aa849bca315c4f31acf24dd85682a4fa3197
Circkoooooo <2389451262@qq.com>
2023-01-22 22:57:27 +0800

    fix: update the layout of the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
 import styled from 'styled-components'
 import Left from './components/layouts/Left'
+import Main from './components/layouts/Main'
 
 const Background = styled.div`
 	width: 100%;
 	height: 100%;
 	background-color: #f0f0f0;
+	display: flex;
 `
 
 function App() {
-	const [isCollapse, setIsCollapse] = useState(false)
-
 	return (
 		<Background>
-			<Left isCollapse={isCollapse} />
+			<Left />
+			<Main />
 		</Background>
 	)
 }
