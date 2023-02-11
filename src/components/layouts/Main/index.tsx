4fbacab0659c23d2713773ebd7ddde0e099b9f9c
Circkoooooo <2389451262@qq.com>
2023-02-11 23:15:49 +0800

    chore: clear

--- src/components/layouts/Main/index.tsx
+++ src/components/layouts/Main/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import Text from '../../elements/common/Text/Text'
-import Linear from '../../elements/layout/Linear'
 import { ElementFunctionComponentType } from '../../elements/type'
 import { MainContainer, MainScreen } from './styled'
 
 interface MainProps {
 	renderedElements?: ElementFunctionComponentType[]
 }
+
 const Main: React.FC<MainProps> = ({ renderedElements }) => {
 	return (
 		<MainContainer>
@@ -14,7 +13,6 @@ const Main: React.FC<MainProps> = ({ renderedElements }) => {
 				{renderedElements?.map((RenderedElement) => {
 					return <RenderedElement key={crypto.randomUUID()} />
 				})}
-				<Linear>123</Linear>
 				{/* TODO: 实现元素嵌套的渲染逻辑。 */}
 			</MainScreen>
 		</MainContainer>
