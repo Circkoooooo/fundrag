77350192e4a597656f3525e8929b3eed7988e87e
Circkoooooo <2389451262@qq.com>
2023-02-17 14:09:25 +0800

    feat: implement data item of right panel

---
+++ src/layouts/Right/DataItem.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+const DataItemContainer = styled.div`
+	width: 100%;
+	margin: 8px 0 16px 0;
+	padding: 0 8px;
+`
+
+const DataItemTitle = styled.div`
+	font-weight: bold;
+	text-overflow: ellipsis;
+	overflow: hidden;
+`
+
+const DataItemEdit = styled.input`
+	margin-top: 4px;
+	width: 100%;
+	outline: none;
+	padding: 10px;
+	border: 1px solid #000;
+	border-radius: 4px;
+`
+
+interface DataItemProps {
+	title?: string
+	value?: string
+}
+
+const DataItem: React.FC<DataItemProps> = ({ title, value }) => {
+	return (
+		<DataItemContainer>
+			<DataItemTitle>{title === undefined ? '未命名' : title}</DataItemTitle>
+			<DataItemEdit defaultValue="空" value={value} />
+		</DataItemContainer>
+	)
+}
+
+export default DataItem
