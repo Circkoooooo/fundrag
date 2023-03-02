52ad0346fc3d51818ae5268525d940d640bf141e
Circkoooooo <2389451262@qq.com>
2023-03-03 00:34:00 +0800

    fix: clear previous codes

--- src/views/elements/index.ts
+++ src/views/elements/index.ts
@@ -12,39 +12,3 @@ export function buildComponentPackage(dragComponentPackage: DragComponentPackage
 }
 
 export const componentPackages: DragComponentPackage[] = [common, layout]
-
-// // const elementBucket: chunkOutput[] = [common]
-//
-// // export default elementBucket
-//
-// // build a base props for drag component, the prop will not include the automally create props such as unique key.
-// export function buildBaseProps(componentType: DragComponentType, componentName: string | number): any {
-// 	let tempProps = {}
-// 	/* eslint-disable */
-// 	switch (componentType) {
-// 		case 'container':
-// 			const baseContainerProps = {
-// 				componentType,
-// 				componentName,
-// 				children: [],
-// 				// defaultStyleProperties,
-// 			}
-// 			baseContainerProps.componentType = 'container'
-//
-// 			tempProps = baseContainerProps
-// 			break
-// 		case 'inline':
-// 			const baseAppendProps = {
-// 				componentType,
-// 				componentName,
-// 			}
-// 			baseAppendProps.componentType = 'inline'
-// 			tempProps = baseAppendProps
-// 			break
-// 		default:
-// 			break
-// 	}
-// 	/* eslint-enable */
-//
-// 	return tempProps
-// }
