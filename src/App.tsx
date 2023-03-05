1a6fc0669e0cba0eeb999e426f42ac6d1613a138
Circkoooooo <2389451262@qq.com>
2023-03-05 14:05:50 +0800

    fix: split logic

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const Background = styled.div`
 //TODO:编写每个每个组件的逻辑单元测试，要求点击为渲染的元素可以渲染内容，点击已渲染的元素切换state等等。
 function App() {
 	const [dragComponentState, setDragComponentState] = useState<DragComponent | null>(null)
-	const [dragComponentTreeRoot, setDragComponentTreeRoot] = useState<DragComponent[]>([])
+	const [dragComponentTree, setDragComponentTree] = useState<DragComponent[]>([])
 
 	//将选中的组件添加到状态中。
 	//需要clone出新对象保证每个元素渲染出不同的实例，可优化。
@@ -32,37 +32,13 @@ function App() {
 			const newDragComponentClone = cloneDragComponent(newDragComponent, componentUniqueKey())
 			if (dragComponentState === null) {
 				setDragComponentState(newDragComponentClone)
-				setDragComponentTreeRoot([...dragComponentTreeRoot, newDragComponentClone])
+				setDragComponentTree([...dragComponentTree, newDragComponentClone])
 			} else {
 				const dragComponentClone = { ...dragComponentState }
 				dragComponentClone.children.appendChild(newDragComponentClone)
 				setDragComponentState(dragComponentClone)
 			}
 		}
-	}
-
-	//渲染出组件的逻辑 嵌套
-	const renderDragComponents = () => {
-		const renderChildLoop = (dragComponent: DragComponent) => {
-			if (dragComponent.children.value.length === 0) {
-				return <dragComponent.RealNode key={dragComponent.key} />
-			}
-			return (
-				<dragComponent.RealNode key={dragComponent.key}>
-					{dragComponent.children.value.map((child) => {
-						return <child.RealNode key={child.key} />
-					})}
-				</dragComponent.RealNode>
-			)
-		}
-
-		return (
-			<>
-				{dragComponentTreeRoot.map((dragComponent) => {
-					return renderChildLoop(dragComponent)
-				})}
-			</>
-		)
 	}
 
 	return (
@@ -78,7 +54,7 @@ function App() {
 				<Left>
 					<ElementSelection onItemClick={(dragComponent) => selectDragComponent(dragComponent)} componentPackages={componentPackages} />
 				</Left>
-				<Main>{renderDragComponents()}</Main>
+				<Main dragComponentTree={dragComponentTree} />
 				<Right />
 			</DragComponentContext.Provider>
 		</Background>
--- src/views/layouts/Main/index.tsx
+++ src/views/layouts/Main/index.tsx
@@ -1,18 +1,45 @@
 import React from 'react'
 import { MainContainer, MainScreen } from './styled'
 import { DragComponentContext } from '../../../context/DragComponentContext'
+import { DragComponent } from '../../../encapsulator'
 
 interface MainProps {
+	dragComponentTree: DragComponent[]
 	children?: React.ReactNode
 }
 
-const Main: React.FC<MainProps> = ({ children }) => {
+const Main: React.FC<MainProps> = ({ children, dragComponentTree }) => {
+	//TODO: state来管理当前已渲染的元素。
+	//渲染出组件的逻辑 嵌套
+	const renderDragComponents = () => {
+		const renderChildLoop = (dragComponent: DragComponent) => {
+			if (dragComponent.children.value.length === 0) {
+				return <dragComponent.RealNode key={dragComponent.key} />
+			}
+
+			return (
+				<dragComponent.RealNode key={dragComponent.key}>
+					{dragComponent.children.value.map((child) => {
+						return <child.RealNode key={child.key} />
+					})}
+				</dragComponent.RealNode>
+			)
+		}
+
+		return (
+			<>
+				{dragComponentTree.map((dragComponent) => {
+					return renderChildLoop(dragComponent)
+				})}
+			</>
+		)
+	}
 	return (
 		<DragComponentContext.Consumer>
 			{({ setCurrentDragComponent }) => {
 				return (
 					<MainContainer>
-						<MainScreen onClick={() => setCurrentDragComponent(null)}>{children}</MainScreen>
+						<MainScreen onClick={() => setCurrentDragComponent(null)}>{renderDragComponents()}</MainScreen>
 					</MainContainer>
 				)
 			}}
